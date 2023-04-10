c2f0960d258ce4816e737bd62b1951ee07f8b8b4
code-bajju <11212766@mmec.mmdu.org>
2023-04-10 15:09:58 +0530

    Data fetching using firebase

--- src/Subcomponents/CardGrid/futureEvents.jsx
+++ src/Subcomponents/CardGrid/futureEvents.jsx
@@ -1,125 +1,191 @@
-import React from 'react'
-import './Card.sass'
-import Image from '../../Assets/images/image.jpg'
-import { Link } from 'react-router-dom'
-function FutureEvents() {
+// import React from 'react'
+// import './Card.sass'
+// import Image from '../../Assets/images/image.jpg'
+// import { Link } from 'react-router-dom'
+// function FutureEvents() {
  
-  const initialState = [
-    {
-        id:12,
-        title: 'Compose Camp',
-        CompleteTitle : 'Compose Camp',
-        details:{
-            date: '01-03-2023',
-            time:{
-                start: '1:40 pm',
-                finish :'4:00 pm'
-            },
-            venue: 'Lab 176, MMEC, MM(DU)'
-        },
-        status: 'upcoming',
-        type: 'Info Session ',
-        img: 'https://blog.desdelinux.net/wp-content/uploads/2021/08/Jetpack-Compose-1024x305.png',
-        description:'Compose Camps are community-organized events focused on how to build Android apps using Jetpack Compose. During these sessions, attendees can get hands-on coding experience on how to develop apps using the latest toolset, Compose, the modern way to develop UI on Android. For those of you who’ve attended Android Study Jams in the past, this is a fun spin on the event series with a focus on Compose!',
-        Speaker:{
-            name:'Ms. Jenny Oliens',
-            type: 'Android developer',
-            image: 'https://blog.desdelinux.net/wp-content/uploads/2021/08/Jetpack-Compose-1024x305.png'
-        },
-        EventImages: ["a","a","a","a",]
-    },
-    {
-        id:13,
-        title: 'Appeofesta',
-        details:{
-            date: '01-04-2023',
-            time:{
-                start: '1:40 pm',
-                finish :'4:00 pm'
-            },
-            venue: 'Seminar Hall Block-2 mmec'
-        },
-        status: 'upcoming',
-        type: 'Hackathon',
-        img: 'https://th.bing.com/th/id/OIP.DX2t5tVds1PRUeKQotQYnAHaFY?pid=ImgDet&rs=1',
-        description:'',
-    },
-    {
-        id:14,
-        title: 'Machine Learning',
-        details:{
-            date: '25-03-2023',
-            time:{
-                start: '1:40 pm',
-                finish :'4:00 pm'
-            },
-            venue: 'Lab 176, MMEC, MM(DU)'
-        },
-        status: 'upcoming',
-        type: 'workshop',
-        img :'https://th.bing.com/th/id/OIP.caPXNH5ZFv06sl-66KFdVQHaE7?pid=ImgDet&rs=1',
-        description:'',
-    },
-    {
-        id:15,
-        title: 'Flutter Festival',
-        details:{
-            date: '15-03-2023',
-            time:{
-                start: '1:40 pm',
-                finish :'4:00 pm'
-            },
-            venue: 'Lab 176, MMEC, MM(DU)'
-        },
-        status: 'upcoming',
-        type: 'workshop',
-        img :'https://miro.medium.com/max/1200/1*qUwhyiEX0ubr7ezTj85ZGQ.jpeg',
-        description:'',
-    },
-]
-console.log(initialState[0])
+//   const initialState = [
+//     {
+//         id:12,
+//         title: 'Compose Camp',
+//         CompleteTitle : 'Compose Camp',
+//         details:{
+//             date: '01-03-2023',
+//             time:{
+//                 start: '1:40 pm',
+//                 finish :'4:00 pm'
+//             },
+//             venue: 'Lab 176, MMEC, MM(DU)'
+//         },
+//         status: 'upcoming',
+//         type: 'Info Session ',
+//         img: 'https://blog.desdelinux.net/wp-content/uploads/2021/08/Jetpack-Compose-1024x305.png',
+//         description:'Compose Camps are community-organized events focused on how to build Android apps using Jetpack Compose. During these sessions, attendees can get hands-on coding experience on how to develop apps using the latest toolset, Compose, the modern way to develop UI on Android. For those of you who’ve attended Android Study Jams in the past, this is a fun spin on the event series with a focus on Compose!',
+//         Speaker:{
+//             name:'Ms. Jenny Oliens',
+//             type: 'Android developer',
+//             image: 'https://blog.desdelinux.net/wp-content/uploads/2021/08/Jetpack-Compose-1024x305.png'
+//         },
+//         EventImages: ["a","a","a","a",]
+//     },
+//     {
+//         id:13,
+//         title: 'Appeofesta',
+//         details:{
+//             date: '01-04-2023',
+//             time:{
+//                 start: '1:40 pm',
+//                 finish :'4:00 pm'
+//             },
+//             venue: 'Seminar Hall Block-2 mmec'
+//         },
+//         status: 'upcoming',
+//         type: 'Hackathon',
+//         img: 'https://th.bing.com/th/id/OIP.DX2t5tVds1PRUeKQotQYnAHaFY?pid=ImgDet&rs=1',
+//         description:'',
+//     },
+//     {
+//         id:14,
+//         title: 'Machine Learning',
+//         details:{
+//             date: '25-03-2023',
+//             time:{
+//                 start: '1:40 pm',
+//                 finish :'4:00 pm'
+//             },
+//             venue: 'Lab 176, MMEC, MM(DU)'
+//         },
+//         status: 'upcoming',
+//         type: 'workshop',
+//         img :'https://th.bing.com/th/id/OIP.caPXNH5ZFv06sl-66KFdVQHaE7?pid=ImgDet&rs=1',
+//         description:'',
+//     },
+//     {
+//         id:15,
+//         title: 'Flutter Festival',
+//         details:{
+//             date: '15-03-2023',
+//             time:{
+//                 start: '1:40 pm',
+//                 finish :'4:00 pm'
+//             },
+//             venue: 'Lab 176, MMEC, MM(DU)'
+//         },
+//         status: 'upcoming',
+//         type: 'workshop',
+//         img :'https://miro.medium.com/max/1200/1*qUwhyiEX0ubr7ezTj85ZGQ.jpeg',
+//         description:'',
+//     },
+// ]
+// console.log(initialState[0])
+
+//   return (
+//       <div className="E-SC-container">
+//          {
+//           initialState.map((item)=>{
+//            return(
+//             <div className="E-SC-1">
+//             <div className="E-SC-1-1">
+//             <img src={item.img} alt="" />
+//             </div>
+//             <div className="E-SC-1-2">
+             
+//                 <table>
+//                   <tr>
+//                     <td colSpan="2" id="head" >{item.title}</td>
+//                   </tr>
+//                   <tr  >
+//                     <td colSpan="2" id="subhead">Details</td>
+//                   </tr>
+//                   <tr>
+//                     <td>Date :</td>
+//                     <td>{item.details.date}</td>
+//                   </tr>
+//                   <tr>
+//                     <td>Time :</td>
+//                     <td> {item.details.time.start} to {item.details.time.finish} </td>
+//                   </tr>
+//                   <tr>
+//                     <td>Venue : </td>
+//                     <td>{item.details.venue}</td>
+//                   </tr>
+//                 </table>
+//                 <button>
+//                   <Link to=''> Register</Link>
+//                 </button>
+//             </div>
+//           </div>
+//            )
+//           })
+//          }
+//       </div>
+//   )
+// }
+
+// export default FutureEvents
+
+
+
+import React, { useState, useEffect } from 'react';
+import './Card.sass';
+import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+function FutureEvents() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const eventsRef = firebase.database().ref('users');
+    eventsRef.on('value', (snapshot) => {
+      const eventsData = snapshot.val();
+      const eventsList = Object.values(eventsData);
+      setEvents(eventsList);
+    });
+  }, []);
 
   return (
-      <div className="E-SC-container">
-         {
-          initialState.map((item)=>{
-           return(
-            <div className="E-SC-1">
-            <div className="E-SC-1-1">
-            <img src={item.img} alt="" />
-            </div>
-            <div className="E-SC-1-2">
-             
-                <table>
-                  <tr>
-                    <td colSpan="2" id="head" >{item.title}</td>
-                  </tr>
-                  <tr  >
-                    <td colSpan="2" id="subhead">Details</td>
-                  </tr>
-                  <tr>
-                    <td>Date :</td>
-                    <td>{item.details.date}</td>
-                  </tr>
-                  <tr>
-                    <td>Time :</td>
-                    <td> {item.details.time.start} to {item.details.time.finish} </td>
-                  </tr>
-                  <tr>
-                    <td>Venue : </td>
-                    <td>{item.details.venue}</td>
-                  </tr>
-                </table>
-                <button>
-                  <Link to=''> Register</Link>
-                </button>
-            </div>
+    <div className="E-SC-container">
+      {events.map((event) => (
+        <div className="E-SC-1" key={event.id}>
+          <div className="E-SC-1-1">
+            <img src={event.imageUrl} alt="" />
           </div>
-           )
-          })
-         }
-      </div>
-  )
+          <div className="E-SC-1-2">
+            <table>
+              <tr>
+                <td colSpan="2" id="head">
+                  {event.completeTitle}
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2" id="subhead">
+                  Details
+                </td>
+              </tr>
+              <tr>
+                <td>Date :</td>
+                <td>{event.date}</td>
+              </tr>
+              <tr>
+                <td>Time :</td>
+                <td>
+                  {event.startTime} to {event.finishTime}
+                </td>
+              </tr>
+              <tr>
+                <td>Venue :</td>
+                <td>{event.venue}</td>
+              </tr>
+            </table>
+            <button>
+              <Link to="">Register</Link>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default FutureEvents
+export default FutureEvents;
