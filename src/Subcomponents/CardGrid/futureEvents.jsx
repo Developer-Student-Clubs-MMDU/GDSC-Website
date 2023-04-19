2f273f2b15da2811e38a90f5627e457db9a447b7
code-bajju <11212766@mmec.mmdu.org>
2023-04-20 00:13:19 +0530

    Firebase Auth

--- src/Subcomponents/CardGrid/futureEvents.jsx
+++ src/Subcomponents/CardGrid/futureEvents.jsx
@@ -1,61 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import './Card.sass';
+import firebase from 'firebase/compat/app';
 import { Link } from 'react-router-dom';
-import firebase from 'firebase/app';
-import 'firebase/database';
 
 function FutureEvents() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    const eventsRef = firebase.database().ref('users');
+    const eventsRef = firebase.database().ref('events');
     eventsRef.on('value', (snapshot) => {
       const eventsData = snapshot.val();
-      const eventsList = Object.values(eventsData);
-      setEvents(eventsList);
+      const eventsArray = [];
+      for (let id in eventsData) {
+        eventsArray.push({ id, ...eventsData[id] });
+      }
+      setEvents(eventsArray);
     });
   }, []);
 
+  // Render the events data
   return (
     <div className="E-SC-container">
-      {events.map((event) => (
-        <div className="E-SC-1" key={event.id}>
-          <div className="E-SC-1-1">
-            <img src={event.imageUrl} alt="" />
+      {events.map((item) => {
+        return (
+          <div className="E-SC-1" key={item.id}>
+            <div className="E-SC-1-1">
+              <img src={item.img} alt="" />
+            </div>
+            <div className="E-SC-1-2">
+              <table>
+                <tbody>
+                  <tr>
+                    <td colSpan="2" id="head">
+                      {item.title}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan="2" id="subhead">
+                      Details
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Date :</td>
+                    <td>{item.details.date}</td>
+                  </tr>
+                  <tr>
+                    <td>Time :</td>
+                    <td>
+                      {' '}
+                      {item.details.time.start} to {item.details.time.finish}{' '}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Venue : </td>
+                    <td>{item.details.venue}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <button>
+                <Link to=""> Register</Link>
+              </button>
+            </div>
           </div>
-          <div className="E-SC-1-2">
-            <table>
-              <tr>
-                <td colSpan="2" id="head">
-                  {event.completeTitle}
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="2" id="subhead">
-                  Details
-                </td>
-              </tr>
-              <tr>
-                <td>Date :</td>
-                <td>{event.date}</td>
-              </tr>
-              <tr>
-                <td>Time :</td>
-                <td>
-                  {event.startTime} to {event.finishTime}
-                </td>
-              </tr>
-              <tr>
-                <td>Venue :</td>
-                <td>{event.venue}</td>
-              </tr>
-            </table>
-            <button>
-              <Link to="">Register</Link>
-            </button>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
