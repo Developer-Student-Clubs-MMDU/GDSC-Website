36f5744666a2a5c44553e82189ea720a2280d3e7
ace <kaurrajvinder736@gmail.com>
2023-05-08 19:50:13 +0530

    data embedded in code

--- src/Pages/Events/eventDetails.jsx
+++ src/Pages/Events/eventDetails.jsx
@@ -17,48 +17,47 @@ function EventDetails() {
           const newData = querySnapshot.docs
               .map((doc) => ({...doc.data(), id:doc.id }));
           setdata(newData);                
-          console.log(data);
+          // console.log(data);
       })}
   
     const array =  data.filter(obj => obj.id == id); 
+    const item = array[0];
     useEffect(()=>{
       Fetch();
-      console.log(id)
-      console.log(array)
     },[]);
 
 
 
     
 
-  const Speaker ={
-    name:'James Olive',
-    detail : 'Android developer',
-    img :'https://i.pinimg.com/originals/bc/67/74/bc6774134132b43d5ec6ed7bdc748a6c.jpg'
-  }
+  // const Speaker ={
+  //   name:'James Olive',
+  //   detail : 'Android developer',
+  //   img :'https://i.pinimg.com/originals/bc/67/74/bc6774134132b43d5ec6ed7bdc748a6c.jpg'
+  // }
 
 
   return (
     <div className='ED-CONTAINER'>
       <div className="ED-1"><h1>Event Details</h1></div>
-      <div className="ED-2"><p>Past Events</p> <BsChevronRight /> <p>Compose Camp</p> </div>
+      <div className="ED-2"><p>Past Events</p> <BsChevronRight /> <p>{item.title}</p> </div>
       <div className="ED-3">
-        <h1>Info Session | Compose Camp</h1>
-        <p>Compose Camps are community-organized events focused on how to build Android apps using Jetpack Compose. During these sessions, attendees can get hands-on coding experience on how to develop apps using the latest toolset, Compose, the modern way to develop UI on Android. For those of you who’ve attended Android Study Jams in the past, this is a fun spin on the event series with a focus on Compose!</p>
+        <h1>{item.type} | {item.completeTitle}</h1>
+        <p>{item.description}</p>
       </div>
       <h2>Speaker</h2>
       <div className="ED-4">
         <div className="ED-4-1">
           <div className="ED-4-1-1">
-            <img src={Speaker.img} alt="" />
-            <h3>{Speaker.name}</h3>
-            <p>{Speaker.detail}</p>
+            <img src={item.speakerImg} alt="" />
+            <h3>{item.speakerName}</h3>
+            <p>{item.speakerType}</p>
           </div>
         </div>
       </div>
       <div className="ED-5">
         <h2>Moments</h2>
-        <Moments/>
+        <Moments />
       </div>
     </div>
   )
