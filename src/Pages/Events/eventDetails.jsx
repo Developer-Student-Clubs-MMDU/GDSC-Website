cae2b6341805d1c61431e556b8af9244caebc185
ace <kaurrajvinder736@gmail.com>
2023-05-08 14:49:17 +0530

    firebase data fetched , id fetched, filtered data

--- src/Pages/Events/eventDetails.jsx
+++ src/Pages/Events/eventDetails.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 import './eventDetails.sass';
 import { BsChevronRight } from 'react-icons/bs';
 import Moments from '../../Subcomponents/Carousels/eventImg/Moments';
+import {db} from '../../Firebase'
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect,useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 function EventDetails() {
+  const {id} = useParams();
+
+    const [data, setdata] = useState([{}]);
+    const Fetch = async()=>{
+      await getDocs(collection(db, "users"))
+      .then((querySnapshot)=>{               
+          const newData = querySnapshot.docs
+              .map((doc) => ({...doc.data(), id:doc.id }));
+          setdata(newData);                
+          console.log(data);
+      })}
+  
+    const array =  data.filter(obj => obj.id == id); 
+    useEffect(()=>{
+      Fetch();
+      console.log(id)
+      console.log(array)
+    },[]);
+
+
+
+    
 
   const Speaker ={
     name:'James Olive',
