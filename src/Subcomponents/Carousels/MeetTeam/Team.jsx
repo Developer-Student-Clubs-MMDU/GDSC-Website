2982377ea4233a90e4fb34c89b0fa35aa6a923c9
ace <kaurrajvinder736@gmail.com>
2023-05-28 19:56:13 +0530

    team carousel made responsive

--- src/Subcomponents/Carousels/MeetTeam/Team.jsx
+++ src/Subcomponents/Carousels/MeetTeam/Team.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowForward } from 'react-icons/io';
 import './Team.sass';
 import { db } from '../../../Firebase'
 import { collection, getDocs } from "firebase/firestore";
@@ -26,42 +28,40 @@ function Team() {
 
 
   const settings = {
-    className: "center",
-    centerMode: true,
     infinite: true,
     dots: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: true,
-    // centerPadding: "100px",
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    nextArrow: <IoIosArrowForward />,
+    prevArrow: <IoIosArrowBack />,
     responsive: [
-      {
-        breakpoint: 1028,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+            }
+        },
+        
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
         }
-      },
-
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          // centerPadding: "80px",
-        }
-      }
     ]
-  };
+};
   return (
     <div className="tag" id="tag">
       <h1> Batch {a.batch}</h1>
