67291470ac3b7f280fe90d4ccd2207ee1a21a29f
ace <kaurrajvinder736@gmail.com>
2023-05-28 19:26:08 +0530

    social media added and content styling changed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,12 @@ import Collab from '../../Subcomponents/Carousels/Collab/Collab';
 import { Link } from 'react-router-dom'
 import Stats from '../../Subcomponents/StatsBar/Stats';
 import { HashLink } from 'react-router-hash-link';
+import { GrInstagram } from 'react-icons/gr'
+import { FiTwitter } from 'react-icons/fi'
+import { RxDiscordLogo} from 'react-icons/rx'
+import { RiYoutubeLine} from 'react-icons/ri'
+import { SlSocialLinkedin} from 'react-icons/sl'
+import { RiGithubLine} from 'react-icons/ri'
 
 
 export default function Home() {
@@ -16,30 +22,43 @@ export default function Home() {
   }
 
   return (
-   <>
+    < div className='main'>
       <div className="H-Container"  >
-      <li>
-        <p>Google Developer Student Clubs,  Maharishi Markandeshwar (Deemed to be University)</p>
-      </li>
-      <li>
-        <p>Empowering Tech Skills, Unleashing Innovation, Learning and Collaboration!</p>
-      </li> 
+        <li>
+          <p> <b>Google Developer Student Clubs</b> ,  Maharishi Markandeshwar (Deemed to be University)</p>
+        </li>
+        <li>
+          <p>Empowering Tech Skills, Unleashing Innovation, Learning and Collaboration!</p>
+        </li>
         <li>
           <Link to={"/#H-Sc-C"} onClick={scroll} >
-          <button> About Us</button>
+            <button>More About Us</button>
           </Link>
         </li>
-         {/* <ol>
-          <li>a</li>
-          <li>b</li>
-          <li>c</li>
-          <li>d</li>
-          <li>e</li>
-         </ol> */}
+        <li className='social'>
+          <td>
+            <Link to="https://www.instagram.com/gdsc_mmdu/" ><GrInstagram/></Link>
+          </td>
+          <td>
+            <Link to="https://twitter.com/gdsc_mmdu" >< FiTwitter/></Link>
+          </td>
+          <td>
+            <Link to="https://discord.com/invite/Gkb4j9rUPD" ><RxDiscordLogo/></Link>
+          </td>
+          <td>
+            <Link to="https://www.youtube.com/channel/UCI-VV1WUuLG-1ptkoqKYf0g" ><RiYoutubeLine/></Link>
+          </td>
+          <td>
+            <Link to="https://www.linkedin.com/company/dscmmdu/" ><SlSocialLinkedin/></Link>
+          </td>
+          <td>
+            <Link to="https://github.com/Developer-Student-Clubs-MMDU" ><RiGithubLine/></Link>
+          </td>
+        </li>
+      </div>
+      <Media />
+      <Stats />
+      <Collab />
     </div>
-    <Media/>
-    <Stats/>
-    <Collab/>
-   </>
   )
 }
