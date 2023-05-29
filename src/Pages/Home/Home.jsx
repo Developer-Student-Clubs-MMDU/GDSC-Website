aed0a8427defe35f391798d2d70ad8f20f86180c
ace <kaurrajvinder736@gmail.com>
2023-05-29 11:01:59 +0530

    cards made responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -22,9 +22,10 @@ export default function Home() {
     prizes: 24
 }
 var b = {
-  workshops : 10,
-  seminars : 12, 
-  Hackathons :3,
+  workshops : "Our interactive workshops equip Indian university students with practical skills and knowledge in various technical domains, empowering them to thrive in the ever-evolving world of technology.",
+  seminars : "Our insightful seminars, organized under Google Developer Student Clubs, invite industry experts to share their expertise, empowering Indian students to stay updated with the latest tech trends and advancements.", 
+  Hackathons :"Our exhilarating hackathons provide a platform for university-level students in India to showcase their skills, collaborate, and build remarkable prototypes within a limited timeframe.",
+  open:" We collaborate with developers worldwide, enhancing software and creating innovative solutions for the Indian tech community.",
 }
 
   const scroll = () => {
