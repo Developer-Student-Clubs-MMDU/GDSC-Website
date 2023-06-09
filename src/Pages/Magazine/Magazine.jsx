24aa0195d758b0a6c211c798327577833c1b3245
code-bajju <11212766@mmec.mmdu.org>
2023-06-10 01:56:39 +0530

    Magazine

--- src/Pages/Magazine/Magazine.jsx
+++ src/Pages/Magazine/Magazine.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./Magazine.css";
 
 function Magazine() {
-    return (
-        <div>
-            <h1>---------------------------</h1>
-            <h1>magazine for gdsc mmdu goes here</h1>
-            <h1>---------------------------</h1>
+  return (
+    <div className="pdf-container">
+      <h1 className="title">The Developer's Notebook</h1>
+      <div className="pdf-wrapper">
+        <div className="pdf-item">
+          <h1>The Developer's Notebook 2019 - 20</h1>
+          <object
+            data="https://viamtechnologies.com/wp-content/uploads/2023/06/Test.pdf"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
         </div>
-    )
+        <div className="pdf-item">
+          <h1>The Developer's Notebook 2020 - 21</h1>
+          <object
+            data="https://viamtechnologies.com/wp-content/uploads/2023/06/test2.pdf"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="pdf-item">
+          <h1>The Developer's Notebook 2021 - 22</h1>
+          <object
+            data="https://viamtechnologies.com/wp-content/uploads/2023/06/test2.pdf"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+        </div>
+
+        <div className="pdf-item">
+        <h1>The Developer's Notebook 2022 - 23</h1>
+
+          <object
+            data="https://viamtechnologies.com/wp-content/uploads/2023/06/Test.pdf"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Magazine
+export default Magazine;
