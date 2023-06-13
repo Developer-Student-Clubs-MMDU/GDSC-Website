6ab658203cad5e64b8e2f2fd27c95dd8c9af01b2
code-bajju <11212766@mmec.mmdu.org>
2023-06-13 11:25:09 +0530

    Removed Meet The Team

--- src/Pages/AboutTeam/AboutTeam.jsx
+++ src/Pages/AboutTeam/AboutTeam.jsx
@@ -5,33 +5,14 @@ import Team from '../../Subcomponents/Carousels/MeetTeam/Team'
 
 
 function AboutTeam() {
-
-    
-//      const location = useLocation()
-
-//     location = {
-//         pathname:"/about",
-//         hash: "#A-4-1"
-//     }
-   
-//     useEffect(()=> {
-        
-//             let elem = document.getElementById("A-4-1")
-//             if (elem) {
-//                 elem.scrollIntoView()
-            
-//             }
-// },[])
-
     return (
         <div className="AT-Container">
             <div className="AT-1" >
-
                 <h1>Team</h1>
             </div>
             <div className="AT-4">
                 <div className="AT-4-1" id='AT-4-1'>
-                <h1>Meet The Team</h1>
+                {/* <h1>Meet The Team</h1> */}
                 </div>
               <Team/>
         </div>
